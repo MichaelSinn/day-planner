21df85a42f1acbca49771b5cb8da5aa5b04c513a
MichaelSinn <michaelsinn.contact@gmail.com>
2022-09-23 21:18:30 -0400

    Added comments and formated the code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/* 
-TODO: 
-    - Wireframe
-    - README.md
-*/
-
-
 const hoursTableEl = $("#hours");
 
 let currentTime = moment();
@@ -22,21 +15,23 @@ let schedule = {
     row6: "",
     row7: "",
     row8: ""
-}
+} // Store the various tasks throughout the day, to be later store in local storage
 
-if (!localStorage.getItem("schedule")){
+if (!localStorage.getItem("schedule")){ // If there is no current schedule in local memory, save the empty schedule
     localStorage.setItem("schedule", JSON.stringify(schedule));
 }else{
-    schedule = JSON.parse(localStorage.getItem("schedule"));
+    schedule = JSON.parse(localStorage.getItem("schedule")); // Get the current schedule
 }
 
-let timer = setInterval(function(){
+let timer = setInterval(function(){ // Update every 10 seconds to see if the hour has changed and if any of the table row's colours need to be updated
     currentTime = moment();
-    for (let i = 0; i < 9; i ++){
+    for (let i = 0; i < 9; i ++){ // Loop through the rows
         let tableRow = $(`#row${i}`);
+        // Remove the current styling
         tableRow.removeClass("past");
         tableRow.removeClass("present");
         tableRow.removeClass("future");
+        // Add the new styling
         if (currentTime.hour() < (i + 9)){
             tableRow.addClass("future");
         }else if(currentTime.hour() === (i+9)){
@@ -47,13 +42,13 @@ let timer = setInterval(function(){
     }
 }, 10000);
 
-for (let i = 0; i < 9; i++){
-    let timeBlock = $("<tr>");
-    let timeCol = $("<td>");
-    let inputCol = $("<td>");
-    let saveCol = $("<td>");
-    let timeBlockTextArea= $("<textarea>");
-    let saveButton = $("<button>");
+for (let i = 0; i < 9; i++){ // Loop through the rows for the first time
+    let timeBlock = $("<tr>"); // Table row
+    let timeCol = $("<td>"); // Time portion of the table row
+    let inputCol = $("<td>"); // Input portion of the table row
+    let saveCol = $("<td>"); // Save portion of the table row
+    let timeBlockTextArea= $("<textarea>"); // Text entry area
+    let saveButton = $("<button>"); // Save button
 
     if (currentTime.hour() < (i + 9)){
         timeBlock.addClass("future");
@@ -63,31 +58,32 @@ for (let i = 0; i < 9; i++){
         timeBlock.addClass("past");
     }
 
-    saveButton.addClass("saveBtn fas fa-save");
+    saveButton.addClass("saveBtn fas fa-save"); // Add the save button icon
 
-    saveCol.append(saveButton);
-    saveCol.addClass("col-1");
+    saveCol.append(saveButton); // Add the button to the section
+    saveCol.addClass("col-1"); // Add styling
 
-    timeBlockTextArea.val(schedule[`row${i}`]);
+    timeBlockTextArea.val(schedule[`row${i}`]); // Add the saved schedule information into text area
     inputCol.append(timeBlockTextArea);
-    inputCol.addClass("col-10");
+    inputCol.addClass("col-10"); // Add styling
 
     timeCol.text(`${i + 9}h`);
-    timeCol.addClass("hour col-1");
+    timeCol.addClass("hour col-1"); // Add styling
 
+    // Add the different aspects of the row to the row
     timeBlock.append(timeCol);
     timeBlock.append(inputCol);
     timeBlock.append(saveCol);
 
-    timeBlock.addClass("time-block row");
+    timeBlock.addClass("time-block row"); // Add styling
     timeBlock.prop("id", `row${i}`);
-    hoursTableEl.append(timeBlock);
+    hoursTableEl.append(timeBlock); // Add the row to the planner
 }
 
-hoursTableEl.on("click", ".saveBtn", function(e){
-    let saveRow = $(this).parent().parent().attr("id");
-    let saveItem = $(this).parent().parent().children().eq(1).children().eq(0);
-    let saveText = saveItem.val();
-    schedule[saveRow] = saveText;
-    localStorage.setItem("schedule", JSON.stringify(schedule));
+hoursTableEl.on("click", ".saveBtn", function(){ // On click of the save button
+    let saveRow = $(this).parent().parent().attr("id"); // The ID of the row that has been saved
+    let saveItem = $(this).parent().parent().children().eq(1).children().eq(0); // The textarea
+    let saveText = saveItem.val(); // The value of the textarea
+    schedule[saveRow] = saveText; // Update the schedule
+    localStorage.setItem("schedule", JSON.stringify(schedule)); // Save the new schedule to local storage
 });
